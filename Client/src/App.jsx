1bde7afc7135213fe9cea92c07425badc4cc0562
holetejas <mrtejashole@gmail.com>
2024-03-01 14:04:47 +0530

    complete create a post page ui

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,30 +1,35 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import Home from './pages/Home'
-import Project from './pages/Project'
-import Signin from './pages/Signin'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import PrivateRoute from './components/PrivateRoute'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Project';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/project" element={<Project/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
         </Route>
-        <Route path="/signin" element={<Signin/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+        </Route>
+
+        <Route path='/project' element={<Projects />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
